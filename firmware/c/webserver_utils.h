ec28fe010b4f293ea0aed3ba474b2e871651f626
caspar <c0de@posteo.de>
2025-08-22 23:17:14 +0200

    refactor: extract utility functions from webserver.c
    Extract three utility functions to new webserver_utils module:
    - url_decode(): URL decoding functionality
    - crc32_calculate(): CRC32 calculation with firmware validation
    - safe_flash_copy(): Safe flash memory copy operations

---
+++ firmware/c/webserver_utils.h
@@ -0,0 +1,13 @@
+#ifndef WEBSERVER_UTILS_H
+#define WEBSERVER_UTILS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+uint32_t crc32_calculate(const uint8_t *data, size_t length);
+
+void safe_flash_copy(void* dest, const void* flash_src, size_t len);
+
+void url_decode(char *dst, const char *src, size_t dst_len);
+
+#endif // WEBSERVER_UTILS_H
