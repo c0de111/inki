e96c34424d9cae32e916089aff3652b59c5d000e
caspar <c0de@posteo.de>
2025-08-29 21:12:47 +0200

    refactor: extract historian utils (time, JSON, request build) to historian_client and include in build

---
+++ firmware/c/historian_client.h
@@ -0,0 +1,19 @@
+#ifndef HISTORIAN_CLIENT_H
+#define HISTORIAN_CLIENT_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef USE_CASE_HISTORIAN
+
+// Build HTTP JSON-RPC request for CCU-Historian
+int historian_build_http_request(char* buffer, size_t buffer_size,
+                                const char* host,
+                                int datapoint_id,
+                                uint64_t start_time_ms,
+                                uint64_t end_time_ms);
+
+#endif // USE_CASE_HISTORIAN
+
+#endif // HISTORIAN_CLIENT_H
+
