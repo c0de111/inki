0a8c4debf12bf4e37917068921e160530765d3ae
caspar <c0de@posteo.de>
2025-10-22 14:57:54 +0200

    default config weathermap

--- firmware/c/weathermap_config.h
+++ firmware/c/weathermap_config.h
@@ -9,7 +9,7 @@ typedef struct {
     double center_lat;      // degrees
     double center_lon;      // degrees
     double half_width_m;    // meters (half span horizontally)
-    uint32_t flags;         // bit flags for optional features (e.g., overlay enable)
+    uint32_t flags;         // config version / feature flags
     char reserved[WEATHERMAP_RESERVED_BYTES];
 } weathermap_config_data_t;
 
@@ -19,4 +19,5 @@ typedef struct {
 } weathermap_config_t;
 #define WEATHERMAP_DEFAULT_CENTER_LAT     53.326
 #define WEATHERMAP_DEFAULT_CENTER_LON     8.532
-#define WEATHERMAP_DEFAULT_HALF_WIDTH_M   5000.0
+#define WEATHERMAP_DEFAULT_HALF_WIDTH_M   6000.0
+#define WEATHERMAP_CONFIG_VERSION         0x574D0002u  // 'WM' + version
